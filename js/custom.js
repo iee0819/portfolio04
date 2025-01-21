913bbbc4a9e467a61521aa40c95d091406b0c941
eun hye <iee0819@naver.com>
2025-01-21 15:22:29 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    const mvs = new Swiper('.main_visual_slide', {
+        loop: true,
+        speed: 1000,
+        effect: "fade",
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.main_visual .arrows .next',
+            prevEl: '.main_visual .arrows .prev',
+        },
+    });
+});
+
+// 모바일 메뉴
+
+$(function () {
+
+    $('.header .mopen').on('click', function () {
+        $('.header .gnb').toggleClass('on')
+    })
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('ul').stop().slideUp();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.header .gnb').removeClass('on')
+        $('.header .gnb>ul>li ul').removeAttr('style')
+    })
+});
